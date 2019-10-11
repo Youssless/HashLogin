4abca796ed46a402d11234cb8ed7a9d1c6d671f0
Youssless <yaseenlahmami98@gmail.com>
2019-10-11 11:26:17 +0100

    getting an error with sha256

--- src/login.cpp
+++ src/login.cpp
@@ -1,10 +1,46 @@
-#include <iostream>
 #include "authlib.h"
 #include "openssl/sha.h"
+#include <sstream>
+#include <fstream>
+#include <iomanip>
 
-int main() {
-  bool auth = true;
+void login(const std::string u, const std::string p);
+std::string sha256(const std::string str);
+
+int main(int argc, char** argv) {
+  std::string args[argc];
+  for (int i = 0; i < argc; i++) {
+    args[i] = argv[i];
+  }
   
+  if (argc == 5) {
+    if (args[1] == "-u" && args[3] == "-p")
+      login(args[2], args[4]);
+  }
+  
+  bool auth = true;
   if (auth) authenticated("user");
   else rejected("user");
+
+  return 0;
+}
+
+void login(const std::string u, const std::string p) {
+  std::cout << u << "\n" << sha256(p) <<std::endl;
+
+}
+
+std::string sha256(const std::string str)
+{
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, str.c_str(), str.size());
+    SHA256_Final(hash, &sha256);
+    std::stringstream ss;
+    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    {
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    }
+    return ss.str();
 }
